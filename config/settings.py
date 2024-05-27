db795a3c2e758f6a51558f14dcf553e6e7e52e51
speedfreak <gdramze@gmail.com>
2024-05-27 17:53:52 +0300

    add models

--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'mailing',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +80,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mailing',
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASS'),
     }
 }
 
@@ -121,3 +128,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+
+EMAIL_HOST_USER = os.getenv('SMTP_USER')
+EMAIL_HOST_PASSWORD = os.getenv('SMTP_PASS')
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
--- mailing/models.py
+++ mailing/models.py
@@ -1,3 +1,77 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Client(models.Model):
+    email = models.EmailField(max_length=254, unique=True, verbose_name='Email')
+    first_name = models.CharField(max_length=30, verbose_name='Имя')
+    last_name = models.CharField(max_length=30, verbose_name='Фамилия')
+    patronym = models.CharField(max_length=30, **NULLABLE, verbose_name='Отчество')
+    comment = models.TextField(verbose_name='Комментарий', **NULLABLE)
+    mailing_id = models.ManyToManyField('mailing.Mailing', verbose_name='Рассылка', related_name='clients')
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name} {self.patronym}'
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+
+class Mailing(models.Model):
+    FREQUENCY = (
+        ('d', 'ежедневно'),
+        ('w', 'еженедельно'),
+        ('m', 'ежемесячно'),
+    )
+
+    STATUS = (
+        ('c', 'создана'),
+        ('s', 'запущена'),
+        ('f', 'завершена')
+    )
+
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    start_time = models.DateTimeField(default=timezone.now, verbose_name='Начало рассылки')
+    finish_time = models.DateTimeField(verbose_name='Конец рассылки')
+    frequency = models.CharField(max_length=1, choices=FREQUENCY, default='d', verbose_name='Периодичность')
+    status = models.CharField(max_length=1, choices=STATUS, default=None, **NULLABLE, verbose_name='Статус')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Рассылка'
+        verbose_name_plural = 'Рассылки'
+
+
+class MailingMessage(models.Model):
+    mailing_id = models.OneToOneField('mailing.Mailing', on_delete=models.CASCADE,
+                                      verbose_name='Рассылка', related_name='message')
+    subject = models.CharField(max_length=100, verbose_name='Тема письма')
+    text = models.TextField(verbose_name='Тело письма')
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        verbose_name = 'Сообщение рассылки'
+        verbose_name_plural = 'Сообщения рассылкок'
+
+
+class MailingAttempt(models.Model):
+    mailing_id = models.OneToOneField('mailing.Mailing', on_delete=models.CASCADE,
+                                        verbose_name='Рассылка', related_name='attempt')
+    latest_attempt = models.DateTimeField(default=timezone.now)
+    status = models.BooleanField(default=False)
+    response = models.TextField(**NULLABLE)
+
+    def __str__(self):
+        return f'{self.latest_attempt} {self.status}'
+
+    class Meta:
+        ordering = ['latest_attempt']
+        verbose_name = 'Попытка рассылки'
+        verbose_name_plural = 'Попытки рассылки'
